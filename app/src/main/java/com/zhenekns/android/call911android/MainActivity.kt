46cda5db428217a775ba36bb72af78ba9baedc5a
zhenekq <zhenek02ss@gmail.com>
2021-04-20 23:39:14 +0300

    start files

---
+++ app/src/main/java/com/zhenekns/android/call911android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zhenekns.android.call911android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
