e55ac176cf03595f87b064d58450ae90bf424496
zhenekq <zhenek02ss@gmail.com>
2021-04-21 00:00:22 +0300

    add TODO

--- app/src/main/java/com/zhenekns/android/call911android/MainActivity.kt
+++ app/src/main/java/com/zhenekns/android/call911android/MainActivity.kt
@@ -2,10 +2,19 @@ package com.zhenekns.android.call911android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var callButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        callButton = findViewById(R.id.my_button)
+        callButton.setOnClickListener{
+            //TODO call 911
+        }
     }
 }
